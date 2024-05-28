417f78954a48ea79e16d767646dd7807094bc7ae
kms <aaaa727@khu.ac.kr>
2024-05-28 23:42:32 +0900

    오늘의 삽질 정리

--- shell/initProxy.sh
+++ shell/initProxy.sh
@@ -1,5 +1,5 @@
 docker run -it -d --name proxyServer --cpuset-cpus=1 --memory 1G \
-  -v /Users/kms/Code/2024.05.study/shell/nginx/proxy-init-script.sh:/usr/local/bin/entrypoint.sh \
-  -v /Users/kms/Code/2024.05.study/shell/nginx/nginx-setting.sh:/home/ubuntu/shell \
+  -v ./nginx/proxy-init-script.sh:/usr/local/bin/entrypoint.sh \
+  -v ./nginx/nginx-setting.sh:/home/ubuntu/shell \
   --entrypoint /usr/local/bin/entrypoint.sh \
   ubuntu
--- shell/nginx/proxy-init-script.sh
+++ shell/nginx/proxy-init-script.sh
@@ -1,20 +1,33 @@
 #!/bin/bash
+# 디버그가 어려운 단점
 
 echo "Starting entrypoint script"
 
 # 업데이트 및 패키지 설치
 apt-get update
-apt-get install -y build-essential libpcre3 libpcre3-dev
+apt-get install -y build-essential libpcre3 libpcre3-dev zlib1g-dev libssl-dev
 apt-get install -y curl
 
 
 # 디렉터리 생성
 mkdir -p /home/ubuntu/apps
 mkdir -p /home/ubuntu/script
+mkdir -p /home/ubuntu/conf
+mkdir -p /home/ubuntu/logs
+mkdir -p /home/ubuntu/run
 
 cd /home/ubuntu/apps
 curl https://nginx.org/download/nginx-1.26.0.tar.gz > nginx.tgz
 tar -zxvf nginx.tgz
 
-echo "Entrypoint script completed"
+mv nginx-1.26.0 nginx
 
+cp nginx/conf/nginx.conf /home/ubuntu/conf/
+
+cd nginx
+./configure --prefix=/home/ubuntu/apps/nginx --pid-path=/home/ubuntu/run/nginx.pid --conf-path=/home/ubuntu/conf/nginx/nginx.conf --error-log-path=/home/ubuntu/logs/nginx/error.log --http-log-path=/home/ubuntu/logs/nginx/access.log --with-http_ssl_module --with-stream --with-http_realip_module --with-http_stub_status_module --with-http_gzip_static_module --with-http_v2_module --with-stream_realip_module
+
+make
+make install
+
+tail -f /dev/null
