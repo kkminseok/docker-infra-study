21d45ec9180e6626c5d112a65ccd44e44e769bfa
kms <aaaa727@khu.ac.kr>
2024-06-18 22:15:16 +0900

    docker compose, dockerFile 정리

--- server/docker-build-run.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-tag=$1
-echo $1
-docker build -t was-server:$1 -f ../shell/server-docker/Dockerfile .
-
-
-docker run -d --name wasServer-dockerfile -p 18081:18081 was-server:1.0.3 
---
+++ server/gateway-docker-build-run.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+tag=$1
+docker build -t gateway-server:1.0.1 -f ../shell/server-docker/gateway/Dockerfile .
+
+docker run -d --net study-test-net --name gwServer-dockerfile -p 18081:18081 -p 8761:8761 gateway-server:1.0.1
